cd43131de7a47c53442493fa0935c437e444745b
isglory <isglory@naver.com>
2023-09-20 15:58:55 +0900

    Fix : origin 변경

--- app/login/login.js
+++ app/login/login.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const login = async (id, pw) => {
     const result = await axios.post('http://front.cau-likelion.org', { id, pw }, {
         headers: {
-            'Access-Control-Allow-Origin': 'http://front.cau-likelion.org',
+            'Access-Control-Allow-Origin': '*',
             // 필요한 경우 다른 CORS 관련 헤더도 추가할 수 있습니다.
             // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
             // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
