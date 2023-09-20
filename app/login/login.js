558157cf97e8343a188231e93cdbe9aadf3b5595
isglory <isglory@naver.com>
2023-09-20 15:52:00 +0900

    Fix : cors 허용

--- app/login/login.js
+++ app/login/login.js
@@ -1,7 +1,14 @@
 import axios from 'axios';
 
 export const login = async (id, pw) => {
-    const result = await axios.post('http://front.cau-likelion.org', { id, pw });
+    const result = await axios.post('http://front.cau-likelion.org', { id, pw }, {
+        headers: {
+            'Access-Control-Allow-Origin': 'http://front.cau-likelion.org',
+            // 필요한 경우 다른 CORS 관련 헤더도 추가할 수 있습니다.
+            // 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            // 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+          },
+    });
 
     return result.data;
 }
